cb32c0c834027c9b77558625bfc5b91e020d684e
STIKA <graysky9@naver.com>
2024-02-27 20:49:21 +0900

    UI BackUp

---
+++ Assets/2. Scripts/JSJ/UIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TMP_Text Info;
+
+    private bool isMinColor = false;
+
+    private void Update()
+    {
+        if (isMinColor == false)
+            TextAnimation(-0.005f);
+        else
+            TextAnimation(0.005f);
+        Debug.Log("돌아가는중");
+    }
+
+    private void TextAnimation(float val)
+    {
+        Color color = Info.color;
+        color.a = color.a + val;
+        Info.color = color;
+
+        if (Info.color.a <= 0)
+        {
+            isMinColor = true;
+            Debug.Log("true로 바뀜");
+        }
+
+        if (Info.color.a >= 1)
+        {
+            isMinColor = false;
+            Debug.Log("false로 바뀜");
+
+        }
+    }
+
+}
