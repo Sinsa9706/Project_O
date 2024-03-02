bb82f1e392f2aa284380d58140a616adce7880c1
hyeoneurai <154484354+hyeoneurai@users.noreply.github.com>
2024-03-03 00:13:26 +0900

    feat : PlayerAnimation
    플레이어 애니메이션 추가

---
+++ Assets/2. Scripts/JSJ/ShopScene/PlayerInput.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class PlayerInput : CharacterController
+{
+    private Camera _camera;
+
+    public static bool isInteraction = false;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+}
