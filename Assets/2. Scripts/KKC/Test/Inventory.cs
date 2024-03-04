69a8209c0a1b03e0a6623f773f9301778d0a906e
Sinsa9706 <ox63023@gmail.com>
2024-03-04 09:27:55 +0900

    feat: SlotUI 추가(작업중), 아이템관련 UI 추가(작업중)

--- Assets/2. Scripts/KKC/Test/Inventory.cs
+++ Assets/2. Scripts/KKC/Test/Inventory.cs
@@ -1,11 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class ItemSlot
+{
+    public ItemData item;
+    public int quantity;
+}
 
 public class Inventory : MonoBehaviour
 {
+    public ItemSlotUI[] uiSlots;
+    public ItemSlot[] slots;
+
+    public GameObject inventoryWindow;
+    public GameObject itemInfo;
+
+    [Header("Selected Item")]
+    private ItemSlot selectedItem;
+    private int selectedItemIndex;
+    public TextMeshProUGUI selectedItemName;
+    public TextMeshProUGUI selectedItemPrice;
+    public TextMeshProUGUI selectedItemDescription;
+
+    [Header("Events")]
+    public UnityEvent onOpenInventory;
+    public UnityEvent onCloseInventory;
+
     public static Inventory Instance = null;
     private Dictionary<int, ItemData> HaveItem = new();
+
     private void Awake()
     {
         if (Instance == null)
@@ -22,6 +50,50 @@ public class Inventory : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        inventoryWindow.SetActive(false);
+        slots = new ItemSlot[uiSlots.Length];
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i] = new ItemSlot();
+            uiSlots[i].index = i;
+            uiSlots[i].Clear();
+        }
+
+        ClearSelectItemWindow();
+    }
+
+    public void OnInventoryButton(InputAction.CallbackContext callbackContext)
+    {
+        if(callbackContext.phase == InputActionPhase.Started)
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        if (inventoryWindow.activeInHierarchy)
+        {
+            inventoryWindow.SetActive(false);
+            onCloseInventory?.Invoke();
+            // 마우스 커서 꺼지기
+        }
+        else
+        {
+            inventoryWindow.SetActive(true);
+            onOpenInventory?.Invoke();
+            // 마우스 커서 켜지기
+        }
+    }
+
+    public bool IsOpen()
+    {
+        return inventoryWindow.activeInHierarchy;
+    }
+
     // 아이템을 얻는 메서드
     // 몬스터를 상호작용 했을 때, 다음 순서에 이 메서드를 사용하면 인벤토리에 아이템이 들어가진다.
     public void AddItem(int id)
@@ -30,11 +102,23 @@ public class Inventory : MonoBehaviour
         {
             ItemData itemData = HaveItem[id];
             itemData.Stack++;
+            UpdateUI();
             return;
             // Slot에서 MaxStack 활용해서 20개씩 소분시키기
         }
         ItemData findItemData = Database.Item.Get(id);
         HaveItem.Add(id, findItemData);
+
+        ItemSlot emptySlot = GetEmptySlot();
+
+        if (emptySlot != null)
+        {
+            emptySlot.item = HaveItem[id];
+            emptySlot.quantity = HaveItem[id].Stack;
+            UpdateUI();
+            return;
+        }
+        return;
     }
 
     // 아이템이 사라지는 메서드 (사용했을 때)
@@ -43,8 +127,60 @@ public class Inventory : MonoBehaviour
         HaveItem.Remove(id);
     }
 
+    private void RemoveSelectedItem()
+    {
+
+    }
+
     public ItemData GetItem(int id)
     {
         return HaveItem[id];
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i].item != null)
+                uiSlots[i].Set(slots[i]);
+            else
+                uiSlots[i].Clear();
+        }
+    }
+
+    ItemSlot GetEmptySlot()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i].item == null)
+                return slots[i];
+        }
+
+        return null;
+    }
+
+    public void SelectItem(int index)
+    {
+        if (slots[index].item == null)
+            return;
+
+        selectedItem = slots[index];
+        selectedItemIndex = index;
+
+        selectedItemName.text = selectedItem.item.Name;
+        selectedItemPrice.text = selectedItem.item.Price.ToString();
+        selectedItemDescription.text = selectedItem.item.Desciption;
+
+        //for(int i = 0; i < selectedItem.item.)
+
+        itemInfo.SetActive(true);
+    }
+
+    private void ClearSelectItemWindow()
+    {
+        selectedItem = null;
+        selectedItemName.text = string.Empty;
+        selectedItemPrice.text = string.Empty;
+        selectedItemDescription.text = string.Empty;
     }
 }
--- Assets/2. Scripts/KKC/UI/ItemSlotUI.cs
+++ Assets/2. Scripts/KKC/UI/ItemSlotUI.cs
@@ -1,8 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemSlotUI : MonoBehaviour
 {
-    
+    public Button button;
+    public Image icon;
+    public TextMeshProUGUI quantityText;
+    private ItemSlot curSlot;
+    private Outline outline;
+
+    public int index;
+
+    public void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
+
+    public void Set(ItemSlot slot)
+    {
+        curSlot = slot;
+        icon.gameObject.SetActive(true);
+        icon.sprite = Resources.Load<Sprite>(slot.item.SpritePath);
+        quantityText.text = slot.quantity >= slot.item.Stack ? slot.quantity.ToString() : string.Empty;
+        // 비어있는 슬롯에 새롭게 표시
+        if (slot.quantity >= slot.item.MaxStack)
+        {
+            index++;
+
+            if(slot != null)
+            {
+                while(slot == null)
+                {
+                    index++;
+                }
+                return;
+            }
+        }
+    }
+
+    public void Clear()
+    {
+        curSlot = null;
+        icon.gameObject.SetActive(false);
+        quantityText.text = string.Empty;
+    }
+
+    public void OnButtonClick()
+    {
+
+    }
 }
