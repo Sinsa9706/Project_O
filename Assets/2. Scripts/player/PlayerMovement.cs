269610e18c8cb4074d320c1bdd345b23b41e3edb
wjj329 <wjj2187@naver.com>
2024-03-05 05:55:22 +0900

    fix. 사운드 작업중

--- Assets/2. Scripts/player/PlayerMovement.cs
+++ Assets/2. Scripts/player/PlayerMovement.cs
@@ -47,6 +47,11 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void OnClick()
+    {
+        MainSoundManager.instance.PlaySFX(8);
+    }
+
     private void Move()
     {
         // Shift 키를 누르고 있으면 boostedSpeed를, 아니면 기본 speed를 사용
